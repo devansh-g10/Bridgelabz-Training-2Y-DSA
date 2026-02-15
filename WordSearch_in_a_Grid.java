d24b4a14fd7d1fd54084051a0f41a5f651ec376c
devansh-ce1 <devanshmaheshwari3011@gmail.com>
2026-02-15 15:53:31 +0530

    Implementaion of Word Search in a grid

--- WordSearch_in_a_Grid.java
+++ WordSearch_in_a_Grid.java
@@ -1,4 +1,4 @@
-class WordSearch {
+class WordSearch_in_a_Grid {
     static boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++)
             for (int j = 0; j < board[0].length; j++)
