9f02d2749ce572be64ed11d4ae8c455a1c3c1b0b
devansh-ce1 <devanshmaheshwari3011@gmail.com>
2026-03-12 11:49:16 +0530

    Add Matix to Araay code in java

---
+++ Java_Arrays_Level_1/Factors.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        int[] factors = new int[100];
+        int index = 0;
+
+        for(int i = 1; i <= number; i++) {
+
+            if(number % i == 0) {
+                factors[index++] = i;
+            }
+        }
+
+        System.out.println("Factors:");
+
+        for(int i = 0; i < index; i++) {
+            System.out.print(factors[i] + " ");
+        }
+    }
+}
